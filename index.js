ded4ff023eaeeadcc519d20465bf909bd33d2c50
abhi02495 <abhi.edu.2495@gmail.com>
2022-05-12 14:48:39 +0530

    index.js changes

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import itemRoutes from './routes/item.js'
 import cors from 'cors';
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
